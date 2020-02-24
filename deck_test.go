3266f6b1513e985244b9ef4dde3f9f8cd13941a0
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2020-02-24 17:51:33 -0300

    Criando mais testes com Go

--- deck_test.go
+++ deck_test.go
@@ -9,3 +9,15 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 }
+
+func TestFirstLastCard(t *testing.T) {
+	d := newDeck()
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected to be Ace of Spades, but found %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected to be Four of Clubs, but found %v", d[len(d)-1])
+	}
+}
