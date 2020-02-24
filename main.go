1564456b79d6f4119aa88752c8db74ea067b5bd6
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2020-02-24 14:43:15 -0300

    Segundo Commit

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-func main() {
-	cards := newDeck()
+import "fmt"
 
-	//cards.print()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+func main() {
+	// cards := newDeck()
+
+	// cards.print()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+
+	cards := newDeck()
+	fmt.Println(cards.toString())
 
 }
