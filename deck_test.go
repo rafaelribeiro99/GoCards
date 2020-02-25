68a564e5a603ef847c5048cd6999b9812ed7bb11
Rafael Luna Ribeiro <rafaelribeiro99@gmail.com>
2020-02-25 14:34:56 -0300

    Outro exemplo de testing

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -20,4 +23,18 @@ func TestFirstLastCard(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected to be Four of Clubs, but found %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
